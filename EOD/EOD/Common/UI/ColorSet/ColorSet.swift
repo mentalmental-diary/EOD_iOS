490dba0c31175b8c43eaa3a60c108475f6b40038
김주영 <rlawndud7@nate.com>
2024-05-03 21:02:46 +0900

    기본 구조 변경 및 메인화면 구성

--- EOD/EOD/Common/UI/ColorSet/ColorSet.swift
+++ EOD/EOD/Common/UI/ColorSet/ColorSet.swift
@@ -13,13 +13,13 @@ import SwiftUI
 public extension UIColor {
     
     /// Primary Color Set
-    public enum Yellow: String, AssetColor {
+    enum Yellow: String, AssetColor {
         case yellow50, yellow100, yellow200, yellow300, yellow400, yellow500, yellow600, yellow700, yellow800, yellow900
     }
     
     /// Greyscale Color Set
     /// - Remark: Light/Dark 테마 컬러셋이 대응되어 있으나 송출앱에서는 Light 모드만을 지원한다.
-    public enum Gray: String, AssetColor {
+    enum Gray: String, AssetColor {
         case gray50, gray100, gray200, gray300, gray400, gray500, gray600, gray700, gray800, gray900
     }
 }
