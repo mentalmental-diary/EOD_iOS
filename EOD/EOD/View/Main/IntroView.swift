5c95eb2ad78341a19a73f9f25be9e66aa5b316fe
김주영 <rlawndud7@nate.com>
2024-07-11 18:26:28 +0900

    intro 뷰 구현

--- EOD/EOD/View/Main/IntroView.swift
+++ EOD/EOD/View/Main/IntroView.swift
@@ -14,14 +14,16 @@ struct IntroView: View {
     @State var initScreen: Bool = true // 초기 웰컴 화면
     
     var body: some View {
-        GeometryReader { geometry in
-            if initScreen {
-                welcomeView(geometry: geometry)
-                    .frame(width: geometry.size.width, height: geometry.size.height)
-            } else {
-                tutorialView(geometry: geometry)
+        NavigationView(content: {
+            GeometryReader { geometry in
+                if initScreen {
+                    welcomeView(geometry: geometry)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                } else {
+                    tutorialView(geometry: geometry)
+                }
             }
-        }
+        })
     }
 }
 
@@ -79,18 +81,83 @@ extension IntroView {
                 Text("튜토리얼 화면 3")
                     .tag(2)
                     .frame(width: geometry.size.width, height: geometry.size.height)
-                
-                // 페이지 4
-                LoginView(viewModel: viewModel)
-                    .tag(3)
             }
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
             
-            PageControlView(currentPage: $currentPage, pages: 4)
-                .padding(.bottom, geometry.safeAreaInsets.bottom)
+            PageControlView(currentPage: $currentPage, pages: 3)
+            
+            HStack(spacing: 11) {
+                Divider()
+                    .frame(maxWidth: .infinity, maxHeight: 1.0)
+                    .overlay(UIColor.Gray.gray100.color)
+                
+                Text("간편 로그인")
+                    .font(size: 16)
+                    .foregroundColor(UIColor.Gray.gray500.color)
+                
+                Divider()
+                    .frame(maxWidth: .infinity, maxHeight: 1.0)
+                    .overlay(UIColor.Gray.gray100.color)
+            }
+            .padding(.horizontal, 22)
+            
+            Spacer().frame(height: 15)
+            
+            HStack {
+                Image("icon_basic") // TODO: 임시 이미지 사용
+                Image("icon_basic") // 임시 이미지 사용
+                Image("icon_basic") // 임시 이미지 사용
+            }
+            .frame(maxWidth: .infinity)
+            
+            Spacer().frame(height: 20)
+            
+            buttonView()
+            
+            Spacer()
         }
+        .padding(.bottom, 34 + geometry.safeAreaInsets.bottom)
         .background(UIColor.CommonBackground.background.color)
         .edgesIgnoringSafeArea([.top, .bottom])
+    }
+}
+
+/// ViewBuilder
+extension IntroView {
+    @ViewBuilder func buttonView() -> some View {
+        VStack(spacing: 12) {
+            NavigationLink(destination: {
+                LazyView(
+                    LoginView(viewModel: viewModel).navigationBarHidden(true)
+                )
+            }, label: {
+                Text("이메일로 로그인")
+                    .font(size: 20)
+                    .foregroundColor(Color.white)
+                    .padding(.vertical, 16)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.black)
+                    .cornerRadius(8.0)
+            })
+            
+            NavigationLink(destination: {
+                LazyView(
+                    SignUpView().navigationBarHidden(true)
+                )
+            }, label: {
+                Text("회원가입")
+                    .font(size: 20)
+                    .foregroundColor(Color.black)
+                    .padding(.vertical, 16)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 1) // 검정색 테두리
+                    )
+            })
+        }
+        .padding(.horizontal, 20)
     }
 }
 
