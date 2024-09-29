53b091f95656659d1a5ec52b46cb1ebd365c5136
김주영 <rlawndud7@nate.com>
2024-09-29 20:24:48 +0900

    바뀐 네이밍 변경

--- EOD/EOD/Model/DiaryEnums.swift
+++ EOD/EOD/Model/DiaryEnums.swift
@@ -7,26 +7,26 @@
 
 import Foundation
 
-public enum EmotionType: Int, CaseIterable, CustomStringConvertible, Decodable {
-    case happy
-    case angry
-    case relax
-    case nomoral
-    case sad
-    case excited
-    case proud
-    case worry
+public enum EmotionType: String, CaseIterable, CustomStringConvertible, Decodable {
+    case happy = "HAPPY"
+    case angry = "ANGRY"
+    case ease = "EASE"
+    case soso = "SOSO"
+    case sad = "SAD"
+    case excited = "EXCITED"
+    case fulfilled = "FULFILLED"
+    case worried = "WORRIED"
     
     public var description: String {
         switch self {
         case .angry: return "화나"
         case .excited: return "신나"
         case .happy: return "행복해"
-        case .nomoral: return "그저 그래"
-        case .proud: return "뿌듯해"
-        case .relax: return "마음 편해"
+        case .soso: return "그저 그래"
+        case .fulfilled: return "뿌듯해"
+        case .ease: return "마음 편해"
         case .sad: return "슬퍼"
-        case .worry: return "걱정이야"
+        case .worried: return "걱정이야"
         }
     }
     
@@ -35,11 +35,11 @@ public enum EmotionType: Int, CaseIterable, CustomStringConvertible, Decodable {
         case .angry: return "icon_angry"
         case .excited: return "icon_excited"
         case .happy: return "icon_happy"
-        case .nomoral: return "icon_nomoral"
-        case .proud: return "icon_proud"
-        case .relax: return "icon_relax"
+        case .soso: return "icon_soso"
+        case .fulfilled: return "icon_fulfilled"
+        case .ease: return "icon_ease"
         case .sad: return "icon_sad"
-        case .worry: return "icon_worry"
+        case .worried: return "icon_worried"
         }
     }
     
@@ -48,11 +48,11 @@ public enum EmotionType: Int, CaseIterable, CustomStringConvertible, Decodable {
         case .angry: return "select_angry"
         case .excited: return "select_excited"
         case .happy: return "select_happy"
-        case .nomoral: return "select_nomoral"
-        case .proud: return "select_proud"
-        case .relax: return "select_relax"
+        case .soso: return "select_soso"
+        case .fulfilled: return "select_fulfilled"
+        case .ease: return "select_ease"
         case .sad: return "select_sad"
-        case .worry: return "select_worry"
+        case .worried: return "select_worried"
         }
     }
 }
