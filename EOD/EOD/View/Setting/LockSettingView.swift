e89fbd9502d930f488a8a9111fb14f445b9085e1
김주영 <rlawndud7@nate.com>
2025-04-06 19:04:15 +0900

    패스워드 입력 화면 공통뷰로 분리

--- EOD/EOD/View/Setting/LockSettingView.swift
+++ EOD/EOD/View/Setting/LockSettingView.swift
@@ -25,9 +25,12 @@ struct LockSettingView: View {
                 Spacer()
                 
                 if viewModel.visiblePwSettingView {
-                    passwordSettingView()
+                    PasswordInputView(password: $viewModel.appPassWord, appendAction: { number in
+                        viewModel.addPassWord(number: number)
+                    }, removeAction: {
+                        viewModel.removePassWord()
+                    })
                 } else {
-                    
                     VStack(alignment: .leading) {
                         Text("비밀번호 설정")
                             .font(type: .omyu, size: 22)
@@ -102,95 +105,6 @@ struct LockSettingView: View {
             .background(UIColor.CommonBackground.background.color)
             
             ToastView(toastManager: viewModel.toastManager)
-        }
-    }
-}
-
-extension LockSettingView {
-    private func passwordSettingView() -> some View {
-        VStack {
-            Text("비밀번호를 입력해주세요!")
-                .font(type: .omyu, size: 20)
-                .foregroundColor(.black)
-            
-            Spacer().frame(height: 24)
-            
-            inputPasswordView()
-            
-            Spacer().frame(height: 100)
-            
-            numberPadView()
-            
-            Spacer().frame(height: 64)
-            
-        }
-        .padding(.top, 40)
-    }
-    
-    private func inputPasswordView() -> some View {
-        HStack(spacing: 7) {
-            Spacer()
-            
-            ForEach(0..<4, id: \.self) { index in
-                if index < viewModel.appPassWord.count {
-                    Image("icon_pw_on")
-                } else {
-                    Image("icon_pw_off")
-                }
-            }
-            
-            Spacer()
-        }
-    }
-    
-    private func numberPadView() -> some View {
-        let numbers = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]
-        ]
-        
-        return VStack(spacing: 20) {
-            ForEach(numbers, id: \.self) { row in
-                HStack(spacing: 20) {
-                    ForEach(row, id: \.self) { num in
-                        numberButton(action: {
-                            viewModel.addPassWord(number: num)
-                        }, number: num)
-                    }
-                }
-            }
-            
-            HStack(spacing: 0) {
-                Spacer().frame(width: 105)
-                
-                numberButton(action: {
-                    viewModel.addPassWord(number: 0)
-                }, number: 0)
-                
-                Spacer().frame(width: 34)
-                
-                Button {
-                    viewModel.removePassWord()
-                } label: {
-                    Image("btn_close_B")
-                        .frame(width: 56)
-                }
-                
-                Spacer().frame(width: 16)
-            }
-        }
-        .padding(.horizontal, 40)
-    }
-    
-    private func numberButton(action: @escaping () -> Void, number: Int) -> some View {
-        Button(action: action) {
-            Text("\(number)")
-                .font(type: .omyu, size: 34)
-                .foregroundColor(UIColor.Gray.gray900.color)
-                .frame(width: 85, height: 85)
-                .background(Color(red: 239/255, green: 239/255, blue: 228/255))
-                .clipShape(Circle())
         }
     }
 }
