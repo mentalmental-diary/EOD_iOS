6d43879615af5da5b0d83d20ba174ded93a38d8f
김주영 <rlawndud7@nate.com>
2025-06-08 23:16:06 +0900

    닉네임 변경 화면 추가

--- EOD/EOD/View/Main/MainViewModel.swift
+++ EOD/EOD/View/Main/MainViewModel.swift
@@ -66,6 +66,8 @@ extension MainViewModel {
         UserDefaults.standard.removeObject(forKey: "gameNotificationTime")
         self.isLogin = false
         self.currentTab = .Home
+        self.inputNickname = ""
+        self.currentUserNickname = ""
     }
     
     func kakaoLoginAction() {
@@ -168,15 +170,21 @@ extension MainViewModel {
     }
     
     /// 닉네임 설정
-    func setNickname() {
+    func setNickname(completion: (() -> Void)? = nil) {
         guard !self.inputNickname.isEmpty else { return }
         networkModel.setUserNickname(nickName: inputNickname, completion: { [weak self] result in
             switch result {
             case .success: // 닉네임 설정 성공
-                self?.isLogin = true
-                self?.showUserInfoSetView = false
-                self?.showStartAlert = true
-                self?.currentUserNickname = self?.inputNickname ?? ""
+                DispatchQueue.main.async {
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                    if self?.isLogin == false {
+                        self?.isLogin = true
+                        self?.showUserInfoSetView = false
+                        self?.showStartAlert = true
+                    }
+                    self?.currentUserNickname = self?.inputNickname ?? ""
+                    completion?()
+                }
             case .failure(let error):
                 self?.toastManager.showToast(message: "닉네임 설정 실패")
                 errorLog("🔴 닉네임 설정 API 실패: \(error.localizedDescription)")
